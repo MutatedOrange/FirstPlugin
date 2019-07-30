0b2ab063a08d27d71eff26b9bcf873a749bd19b1
MutatedOrange <37354655+MutatedOrange@users.noreply.github.com>
2019-07-29 18:36:59 -0700

    Add files via upload

---
+++ PunchCounter.java
@@ -0,0 +1,58 @@
+package me.MutatedOrange.FirstPlugin.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import me.MutatedOrange.FirstPlugin.Utils;
+
+public class PunchCounter implements Listener, CommandExecutor {
+	int blocksPunched;
+
+	String igprefix = "§1[§bFirstPlugin§1]§7: §e";
+	//listens for if player is interacting
+	@EventHandler
+	public void onInteract(PlayerInteractEvent event) {
+
+		Action action = event.getAction();
+
+		//tests if player is left clicking a block
+		if(action.equals(Action.LEFT_CLICK_BLOCK)) {
+			
+			Player player = event.getPlayer();
+			
+			addPunches(1);
+			
+			player.sendMessage(Utils.chat("debug: " + blocksPunched));
+			
+			
+		}	
+	}
+	
+	public int addPunches(int numToAdd) {		
+		blocksPunched = blocksPunched + numToAdd;
+		return blocksPunched;		
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {	
+		
+		if(sender instanceof Player) {
+		if(label.equalsIgnoreCase("punches")) {
+			
+			Player player = (Player) sender;
+			addPunches(0);
+			player.sendMessage(Utils.chat(igprefix + "You have punched a block &8" + blocksPunched + "&e Times!"));
+			
+			
+		}
+		}
+		return true;
+	}
+	
+}
